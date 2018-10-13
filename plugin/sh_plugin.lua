42efc6b248a3ee5af683af48abdde16790c0a867
svensis <11571941+Heldex@users.noreply.github.com>
2018-10-13 22:06:28 +0300

    you dont need cl when your not using it
    you don't need cl when your not using it

--- plugin/sh_plugin.lua
+++ plugin/sh_plugin.lua
@@ -1,7 +1,5 @@
 local PLUGIN = PLUGIN;
 local Clockwork = Clockwork;
 
-Clockwork.kernel:IncludePrefixed("cl_plugin.lua");
-Clockwork.kernel:IncludePrefixed("cl_hooks.lua");
 Clockwork.kernel:IncludePrefixed("sv_plugin.lua");
 Clockwork.kernel:IncludePrefixed("sv_hooks.lua");
